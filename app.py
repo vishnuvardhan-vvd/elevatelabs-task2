118d520b80ad6ad37b30d9c6e405fb2343af0d5f
vishnuvardhan-vvd <vishnuvardhan8503@gmail.com>
2025-04-09 11:20:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World! This is a basic CI/CD pipeline demo.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
